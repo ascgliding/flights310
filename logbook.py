436174af42c12912d014ebdd98d5f3580d41d679
rayb <ray.burns@velocityglobal.co.nz>
2023-11-20 08:02:56 +1300

    Add tabs to logbook spreadsheet for towing and tug only.

--- logbook.py
+++ logbook.py
@@ -267,6 +267,27 @@ def export():
     if thisuser is None:
         flash("Your userid is not associated with a pilot.  Sorry about that...")
         render_template("index.html")
+    # Create the workbook
+    filename = os.path.join(app.instance_path,
+                            "downloads/LOGBOOK_" + thisuser.fullname.replace(" ", "_") + ".xlsx")
+    workbook = xlsxwriter.Workbook(filename)
+    #  Add the glider flights
+    add_glider_flights_worksheet(workbook,startdate,enddate,thisuser)
+    add_tows_worksheet(workbook,startdate,enddate,thisuser)
+    add_tugonly_workksheet(workbook,startdate,enddate,thisuser)
+    workbook.close()
+    return send_file(filename, as_attachment=True)
+    return render_template('logbook/logbook.html')
+
+
+
+def add_glider_flights_worksheet(workbook,startdate,enddate,thisuser):
+    """
+    Creates a spreadsheet ready for download for flights between two dates.
+    :param self:
+    :param flights:  List of flights to write
+    :return: Name of excel file.
+    """
     # Get this user:
     sql = sqltext("""
         SELECT
@@ -288,7 +309,9 @@ def export():
                 WHEN t1.seat_count = 2 AND t0.p2 = '' THEN 'P1'
                 WHEN t1.seat_count = 2 AND t2.instructor = 1 THEN 'DI' 
                 ELSE 'P1'
-            END crew_capacity
+            END crew_capacity,
+            (tow_charge + glider_charge + other_charge )  due,
+            t0.paid
             FROM flights t0
             LEFT OUTER JOIN aircraft t1 ON t0.ac_regn = t1.regn
             LEFT OUTER JOIN pilots t2 ON t2.userid = :pilot
@@ -296,6 +319,7 @@ def export():
             and t0.flt_date >= :startdate
             and t0.flt_date <= :enddate 
             and t0.linetype = 'FL'
+            and t0.ac_regn != 'TUG ONLY'
         """)
     sql = sql.columns(id=db.Integer,
                       flt_date=db.Date,
@@ -307,25 +331,20 @@ def export():
                       landed=db.Time,
                       totalmins=db.Integer,
                       seat_count=db.Integer,
-                      crew_capacity=db.String
-                      )
+                      crew_capacity=db.String,
+                      due = SqliteDecimal(10, 2),
+                      paid = db.Boolean
+                    )
     flights = db.engine.execute(sql, startdate=startdate, enddate=enddate, pilot=thisuser.userid).fetchall()
-    return send_file(createxlsx(flights,thisuser.fullname), as_attachment=True)
-    return render_template('logbook/logbook.html')
-
-def createxlsx(flights,user):
-    """
-    Creates a spreadsheet ready for download for flights between two dates.
-    :param self:
-    :param flights:  List of flights to write
-    :return: Name of excel file.
-    """
+    # don't add the worksheet if there are no glider flights...
+    if len(flights) == 0:
+        return
     row = 0
     try:
-        filename = os.path.join(app.instance_path,
-                                "downloads/LOGBOOK_" + user.replace(" ","_") + ".xlsx")
-        workbook = xlsxwriter.Workbook(filename)
-        ws = workbook.add_worksheet()
+        # filename = os.path.join(app.instance_path,
+        #                         "downloads/LOGBOOK_" + user.replace(" ","_") + ".xlsx")
+        # workbook = xlsxwriter.Workbook(filename)
+        ws = workbook.add_worksheet('Glider Flights')
         borderdict = {'border': 1}
         datedict = {'num_format': 'dd-mmm-yy'}
         timedict = {'num_format': 'h:mm'}
@@ -345,6 +364,7 @@ def createxlsx(flights,user):
         ws.write("H3", "Landed", border_fmt)
         ws.write("I3", "Crew Cap", border_fmt)
         ws.write("J3", "Mins", border_fmt)
+        ws.write("K3", "Charge", border_fmt)
         row = 3
         for f in flights:
             ws.write(row, 0, f.id, border_fmt)
@@ -357,15 +377,211 @@ def createxlsx(flights,user):
             ws.write(row, 7, f.landed, time_fmt )
             ws.write(row, 8, f.crew_capacity, border_fmt )
             ws.write(row, 9, f.totalmins, border_fmt )
+            ws.write(row, 10, f.due, dollar_fmt )
             row += 1
         ws.write(row + 1, 1, "Totals:", border_fmt)
         ws.write_formula(row + 1, 9, "sum(J4:J" + str(row) + ")", border_fmt)
+        ws.write_formula(row + 1, 10, "sum(K4:K" + str(row) + ")", dollar_fmt)
         # col widths
-        for i,v in enumerate([3,12,20,20,10,20,12,9,9,9,9,9,12,9,12,12,12,20]):
+        for i,v in enumerate([3,12,20,20,20,20,12,9,9,9,9,9,12,9,12,12,12,25,15]):
             ws.set_column(i,i,v)
     except Exception as e:
         applog.error("An error ocurred during spreadsheet create:{}".format(str(e)))
         applog.debug("Row was:{}".format(row))
         raise
-    workbook.close()
-    return filename
+    # workbook.close()
+    # return filename
+
+def add_tows_worksheet(workbook, startdate, enddate, thisuser):
+        """
+        Creates a spreadsheet ready for download for flights between two dates.
+        :param self:
+        :param flights:  List of flights to write
+        :return: Name of excel file.
+        """
+        towdetail = sqltext("""
+            SELECT
+                t0.id,
+                t0.flt_date,
+                t0.pic,
+                t0.p2,
+                COALESCE(t1.TYPE,"Unknown") actype,
+                ac_regn,
+                time(takeoff) takeoff,
+                time(tug_down) landed,
+                coalesce(round((julianday(t0.tug_down) - julianday(t0.takeoff)) * 1440,0),0) towmins
+                FROM flights t0
+                LEFT OUTER JOIN aircraft t1 ON t0.ac_regn = t1.regn
+                LEFT OUTER JOIN pilots t2 ON t2.userid = :pilot
+                WHERE upper(t2.fullname) = t0.tow_pilot
+                and (t0.flt_date >= :startdate
+                    and t0.flt_date <= :enddate)
+                and t0.linetype = 'FL'
+                and ac_regn != 'TUG ONLY'
+                and tug_regn not in ('SELF LAUNCH', 'WINCH')
+        """)
+        towdetail = towdetail.columns(id=db.Integer,
+                          flt_date=db.Date,
+                          pic=db.String,
+                          p2=db.String,
+                          actype=db.String,
+                          ac_regn=db.String,
+                          takeoff=db.Time,
+                          landed=db.Time,
+                          towmins=db.Integer
+                          )
+        flights = db.engine.execute(towdetail, startdate=startdate, enddate=enddate, pilot=thisuser.userid).fetchall()
+        if len(flights) == 0:
+            return
+        row = 0
+        try:
+            # filename = os.path.join(app.instance_path,
+            #                         "downloads/LOGBOOK_" + user.replace(" ","_") + ".xlsx")
+            # workbook = xlsxwriter.Workbook(filename)
+            ws = workbook.add_worksheet('Glider Tows')
+            borderdict = {'border': 1}
+            datedict = {'num_format': 'dd-mmm-yy'}
+            timedict = {'num_format': 'h:mm'}
+            dollardict = {'num_format': '$#, ##0.00'}
+            border_fmt = workbook.add_format({'border': 1})
+            merge_format = workbook.add_format({'align': 'center', 'border': 1})
+            date_format = workbook.add_format(dict(datedict, **borderdict))
+            dollar_fmt = workbook.add_format(dict(dollardict, **borderdict))
+            time_fmt = workbook.add_format(dict(timedict, **borderdict))
+            ws.write("A3", "Id", border_fmt)
+            ws.write("B3", "Date", border_fmt)
+            ws.write("C3", "Type", border_fmt)
+            ws.write("D3", "Glider Reg", border_fmt)
+            ws.write("E3", "PIC", border_fmt)
+            ws.write("F3", "P2", border_fmt)
+            ws.write("G3", "Take Off", border_fmt)
+            ws.write("H3", "Landed", border_fmt)
+            ws.write("I3", "Mins", border_fmt)
+            row = 3
+            for f in flights:
+                ws.write(row, 0, f.id, border_fmt)
+                ws.write(row, 1, f.flt_date.strftime("%d/%m/%Y"), border_fmt)
+                ws.write(row, 2, f.actype, border_fmt)
+                ws.write(row, 3, f.ac_regn, border_fmt)
+                ws.write(row, 4, f.pic, border_fmt)
+                ws.write(row, 5, f.p2, border_fmt)
+                ws.write(row, 6, f.takeoff, time_fmt)
+                ws.write(row, 7, f.landed, time_fmt)
+                ws.write(row, 8, f.towmins, border_fmt)
+                row += 1
+            ws.write(row + 1, 1, "Totals:", border_fmt)
+            ws.write_formula(row + 1, 8, "sum(I4:I" + str(row) + ")", border_fmt)
+            # col widths
+            for i, v in enumerate([3, 12, 20, 20, 20, 20, 12, 9, 9, 9, 9, 9, 12, 9, 12, 12, 12, 20]):
+                ws.set_column(i, i, v)
+        except Exception as e:
+            applog.error("An error ocurred during spreadsheet create:{}".format(str(e)))
+            applog.debug("Row was:{}".format(row))
+            raise
+
+
+def add_tugonly_workksheet(workbook, startdate, enddate, thisuser):
+    """
+    Creates a spreadsheet ready for download for flights between two dates.
+    :param self:
+    :param flights:  List of flights to write
+    :return: Name of excel file.
+    """
+    tugonly = sqltext("""
+                SELECT
+            t0.id,
+            t0.flt_date,
+            t0.pic,
+            t0.p2,
+            COALESCE(t1.TYPE,"Unknown") actype,
+            CASE WHEN ac_regn = 'TUG ONLY'
+                THEN tug_regn
+                ELSE ac_regn
+            END regn,
+            time(takeoff) takeoff,
+            time(landed) landed,
+            coalesce(round((julianday(t0.landed) - julianday(t0.takeoff)) * 1440,0),0) totalmins,
+            coalesce(t1.seat_count,0) seat_count,
+            CASE WHEN t0.p2 = t2.fullname THEN 'P2'
+                WHEN t1.seat_count = 1 THEN 'P1'
+                WHEN t1.seat_count = 2 AND t0.p2 = '' THEN 'P1'
+                WHEN t1.seat_count = 2 AND t2.instructor = 1 THEN 'DI' 
+                ELSE 'P1'
+            END crew_capacity,
+            (tow_charge + glider_charge + other_charge )  due,
+            t0.paid
+            FROM flights t0
+            LEFT OUTER JOIN aircraft t1 ON t0.tug_regn = t1.regn
+            LEFT OUTER JOIN pilots t2 ON t2.userid = :pilot
+            WHERE (t2.fullname = t0.pic OR t2.fullname = t0.p2)
+            and ((t0.flt_date >= :startdate
+                and t0.flt_date <= :enddate)
+                or (due != 0 and paid == 0) )
+            and t0.linetype = 'FL'
+            and ac_regn = 'TUG ONLY'
+    """)
+    tugonly = tugonly.columns(id=db.Integer,
+                              flt_date=db.Date,
+                              pic=db.String,
+                              p2=db.String,
+                              actype=db.String,
+                              regn=db.String,
+                              takeoff=db.Time,
+                              landed=db.Time,
+                              totalmins=db.Integer,
+                              seat_count=db.Integer,
+                              crew_capacity=db.String,
+                              due=SqliteDecimal(10, 2),
+                              paid=db.Boolean
+                              )
+    flights = db.engine.execute(tugonly, startdate=startdate, enddate=enddate, pilot=thisuser.userid).fetchall()
+    if len(flights) == 0:
+        return
+    row = 0
+    try:
+        # filename = os.path.join(app.instance_path,
+        #                         "downloads/LOGBOOK_" + user.replace(" ","_") + ".xlsx")
+        # workbook = xlsxwriter.Workbook(filename)
+        ws = workbook.add_worksheet('Private Tug')
+        borderdict = {'border': 1}
+        datedict = {'num_format': 'dd-mmm-yy'}
+        timedict = {'num_format': 'h:mm'}
+        dollardict = {'num_format': '$#, ##0.00'}
+        border_fmt = workbook.add_format({'border': 1})
+        merge_format = workbook.add_format({'align': 'center', 'border': 1})
+        date_format = workbook.add_format(dict(datedict, **borderdict))
+        dollar_fmt = workbook.add_format(dict(dollardict, **borderdict))
+        time_fmt = workbook.add_format(dict(timedict, **borderdict))
+        ws.write("A3", "Id", border_fmt)
+        ws.write("B3", "Date", border_fmt)
+        ws.write("C3", "Type", border_fmt)
+        ws.write("D3", "Glider Reg", border_fmt)
+        ws.write("E3", "PIC", border_fmt)
+        ws.write("F3", "P2", border_fmt)
+        ws.write("G3", "Take Off", border_fmt)
+        ws.write("H3", "Landed", border_fmt)
+        ws.write("I3", "Mins", border_fmt)
+        ws.write("J3", "Charge", border_fmt)
+        row = 3
+        for f in flights:
+            ws.write(row, 0, f.id, border_fmt)
+            ws.write(row, 1, f.flt_date.strftime("%d/%m/%Y"), border_fmt)
+            ws.write(row, 2, f.actype, border_fmt)
+            ws.write(row, 3, f.regn, border_fmt)
+            ws.write(row, 4, f.pic, border_fmt)
+            ws.write(row, 5, f.p2, border_fmt)
+            ws.write(row, 6, f.takeoff, time_fmt)
+            ws.write(row, 7, f.landed, time_fmt)
+            ws.write(row, 8, f.totalmins, border_fmt)
+            ws.write(row, 9, f.due, dollar_fmt)
+            row += 1
+        ws.write(row + 1, 1, "Totals:", border_fmt)
+        ws.write_formula(row + 1, 8, "sum(I4:I" + str(row) + ")", border_fmt)
+        ws.write_formula(row + 1, 9, "sum(J4:J" + str(row) + ")", dollar_fmt)
+        # col widths
+        for i, v in enumerate([3, 12, 20, 20, 20, 20, 12, 9, 9, 9, 9, 9, 12, 9, 12, 12, 12, 20,15]):
+            ws.set_column(i, i, v)
+    except Exception as e:
+        applog.error("An error ocurred during spreadsheet create:{}".format(str(e)))
+        applog.debug("Row was:{}".format(row))
+        raise
